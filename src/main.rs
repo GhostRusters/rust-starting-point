78fe4a82cce32679308606db0e78928359197bb4
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2016-08-30 20:48:27 +0200

    Using git.control Atom package

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess");
+    let randomm = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", randomm);
+    loop {
+      let mut guess = String::new();
+      io::stdin().read_line(&mut guess).expect("Failed to read line");
+      let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+      };
+      match guess.cmp(&randomm) {
+        Ordering::Less => println!("Go UP"),
+        Ordering::Equal => {
+          println!("Yeeshhhh!!!");
+          break;
+        },
+        Ordering::Greater => println!("Go DOWN"),
+      }
+      println!("You guessed {}", guess);
+    }
+}
