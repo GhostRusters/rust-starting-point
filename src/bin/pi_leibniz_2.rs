407eecbd3236081a81de1022de7ad58726be14e2
rusted-and-bald <marcos.tengelmann@gmail.com>
2016-09-09 10:45:08 +0100

    Version 2 added.

---
+++ src/bin/pi_leibniz_2.rs
@@ -0,0 +1,17 @@
+use std::f64;
+
+fn main() {
+     let nums = match std::env::args().nth(1) {
+         Some(param) => param.parse::<i32>().unwrap(),
+         None => 0,
+     };
+
+    println!("{:?}", nums);
+
+    let mut pi_value = 0.0;
+    for i in 1..nums {
+        let mut base = 4.0 / ( (2.0 * i as f64) - 1.0 );
+        if i % 2 == 0 {pi_value += base *-1.0;} else {pi_value += base;}
+    }
+    println!("Calculated PI Value is:{:?}", pi_value );
+}
